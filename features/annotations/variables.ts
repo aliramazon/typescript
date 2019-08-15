b3b031053bc2ae53f0e8be0704e0968a21597c09
Safarali Ramazonov <sramazonov@proofpoint.com>
2019-08-15 10:45:53 -0600

    Type annotations for numbers, arrays, classess, objects

---
+++ features/annotations/variables.ts
@@ -0,0 +1,11 @@
+// :number is type annotations. If we replace 5 with other values, we get error.
+const apples: number = 5;
+
+// The same with if we use let and re assign the variable to different type.
+let oranges: number = 5; // oranges = [] does not work.
+
+let speed: string = 'fast'; // speed = 10 does not work
+
+let nothingMuch: null = null;
+
+let now: Date = new Date();
