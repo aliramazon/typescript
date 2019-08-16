3bf5722f233752d107fad529bfa2487538c3d903
Safarali Ramazonov <mr.safarali@gmail.com>
2019-08-16 10:07:20 -0600

    Outline disadvantages of tuples

--- features/annotations/tuples.ts
+++ features/annotations/tuples.ts
@@ -8,3 +8,5 @@ type Drink = [string, boolean, number];
 
 const pepsi: Drink = ['brown', true, 40];
 const sprite: Drink = ['white', true, 50];
+
+const carSpecs: [number, number] = [400, 3354]; // it does not give any idea what the numbers are?
