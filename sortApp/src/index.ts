4c17e1556cabdb1c485929bacdd615a00b31cdf8
Safarali Ramazonov <mr.safarali@gmail.com>
2019-10-04 10:04:36 -0600

    Inherit from Sorter abstract class and sort

--- sortApp/src/index.ts
+++ sortApp/src/index.ts
@@ -5,14 +5,12 @@ import { LinkedList } from './LinkedList';
 
 console.log('======== NUMBERS COLLECTION SORTING =========');
 const numbersCollection = new NumbersCollection([1, 2, -4, 5, 12, 0]);
-const numberSorter = new Sorter(numbersCollection);
-numberSorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 console.log('======== CHARACTERS COLLECTION SORTING =========');
-const charsCollection = new CharactersCollection('wehrbfWdRgdbDgxbhZZAassp');
-const charsSorter = new Sorter(charsCollection);
-charsSorter.sort();
+const charsCollection = new CharactersCollection('AehrbfWdRgdbDgxbhZZAAssp');
+charsCollection.sort();
 console.log(charsCollection.data);
 
 console.log('======== LINKED LIST SORTING =========');
@@ -24,6 +22,5 @@ linkedList.add(5);
 linkedList.add(1000);
 linkedList.add(1);
 
-const linkedListSorter = new Sorter(linkedList);
-linkedListSorter.sort();
+linkedList.sort();
 linkedList.print();
